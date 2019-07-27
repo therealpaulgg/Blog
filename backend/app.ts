ae3b61dd7ee3902398c0b1b94940dc131f78ca9a
therealpaulgg <paul@paul.systems>
2019-07-27 15:40:48 -0700

    initial boilerplate

---
+++ backend/app.ts
@@ -0,0 +1,7 @@
+import express from "express"
+
+const app: express.Application = express()
+
+app.get("/", (req, res) => res.send("Hello world"))
+
+app.listen(3000, () => console.log("Listening on port 3000"))
